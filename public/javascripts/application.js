86c921bca311cd1c9ed5fd47acf4bc2df8f46e91
Eric MSP Veith <eveith@veith-m.de>
2009-02-17 16:32:34 +0100

    Fixed texture display size bug
    - Fixed the texture display size bug in the dropdown menus
    - Added a better dropdown show/hide code
    - Mat dimensions are now displayed in the "Mat Control" window

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -41,3 +41,64 @@ function display_dropdown_prompt(id) {
 function hide_dropdown_prompt(id) {
 	$(id).style.display = "none";
 }
+
+/**
+ * This function scales an element if its size exceeds certain values in width
+ * or height. While doing this, it keeps the original aspect ratio.
+ * @param element The element that is to be scaled
+ * @param width Maximum width, -1 indicates that the width does not matter
+ * @param height Maximum height, -1 indicates that the height does not matter
+ */
+function scale_if_bigger_than(element, max_width, max_height) {
+	var dimensions = element.getDimensions;
+	var ratio = dimensions.height / dimensions.width;
+	var new_dimensions = {
+		width: dimensions.width,
+		height: dimensions.height
+	};
+	
+	if(-1 == max_width) max_width = dimensions.width;
+	if(-1 == max_height) max_height = dimensions.height;
+	
+	
+	if((dimensions.width - max_width) 
+			> (dimensions.height - max_height)) {
+		/* Its broader than high, so scale because of width. */
+		new_dimensions.width = max_width;
+		new_dimensions.height = max_width * ratio; 
+	} else {
+		/* Its higher than broad, scale because of height. */
+		new_dimensions.height = max_height;
+		new_dimensions.width = max_height * ratio;
+	}
+	
+	element.style.width = new_dimensions.width + "px";
+	element.style.height = new_dimensions.height + "px";
+}
+
+
+/**
+ * The currently shown dropdown
+ */
+var current_dropdown_id = null;
+
+/**
+ * Toggles a dropdown prompt. If no prompt is shown, it displays the clicked.
+ * If there is already one shown, it hides the old one and displays the new.
+ * If the user didn't click on any new prompt, the old one is hidden, but no
+ * new one shown.
+ * @param id The prompt's HTML id.
+ */
+function toggle_dropdown(id) {
+	var new_dropdown_id = null;
+	
+	if(null != id && id != current_dropdown_id) {
+		$(id).style.display = "block";
+		new_dropdown_id = id;
+	}
+	
+	if(null != current_dropdown_id)
+		$(current_dropdown_id).style.display = "none";
+	
+	current_dropdown_id = new_dropdown_id;
+}
