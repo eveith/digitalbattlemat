e2389d7d4ee4d1a05ca1b288066a9ecfd0ecd3d6
Eric MSP Veith <eveith@emerelle.veith-m.de>
2009-04-14 00:13:48 +0200

    Battle Mat background now gets saved, too.

--- app/controllers/mats_controller.rb
+++ app/controllers/mats_controller.rb
@@ -17,16 +17,26 @@ class MatsController < ApplicationController
   end
   
   def new
-    redirect_to :action => 'index'    
+    redirect_to :action => 'index'
   end
   
   def edit
     @mat = BattleMats.find_by_id(params[:id])
+    if not @mat
+      redirect_to :action => "index"
+    end
+
     @backgrounds = []
-    @tiles = []
     Dir.foreach('public/images/textures/backgrounds') do |entry|
       if entry =~ /\.(png|jp.?g)$/i then
         @backgrounds << entry
+      end
+    end
+    
+    @tiles = []
+    Dir.foreach('public/images/textures/tiles') do |entry|
+      if entry =~ /\.(png|jp.?g)$/i then
+        @tiles << entry
       end
     end
   end
@@ -36,7 +46,18 @@ class MatsController < ApplicationController
     mat.description = params[:mat][:description]
     mat.x_dimension = params[:mat][:x_dimension]
     mat.y_dimension = params[:mat][:y_dimension]
+    
+    # Remove "url(...)" from background argument
+    if params[:mat][:background] and 
+        params[:mat][:background] =~ /url\(([^)]*)\)/ then
+      mat.background = $1
+    end
     mat.save
+    
+    # Save tiles of this mat
+    puts "---"
+    puts params[:mat][:tiles]["1"]["1"]
+    puts "---"
   end
 
   def show
--- public/javascripts/mat_edit_controls.js
+++ public/javascripts/mat_edit_controls.js
@@ -10,7 +10,11 @@ function show_ajax_failure_window(request) {
 }	
 
 
-function save_mat(form, mat) {
+/**
+ * Saves a complete battle mat, along with everything that belongs to it.
+ * @param form The submitting form
+ */
+function save_mat(form) {
 	var action = form.action;
 	var method = form.method;
 	var mat = $("mini-mat");
@@ -24,15 +28,47 @@ function save_mat(form, mat) {
 	}
 	
 	// Create artifical form elements for the Mat dimensions (total)
-	var dimensions = "mat[x_dimension]=" + mat.rows.length +
-		"&mat[y_dimension]=" + mat.rows[0].cells.length;
+	var dimensions = encodeURI("mat[x_dimension]=" + mat.rows.length +
+		"&mat[y_dimension]=" + mat.rows[0].cells.length);
+	
+	// Encode mat background, too
+	var background = "";
+	if($("mat-mini-container").style.backgroundImage) {
+		background = encodeURI("mat[background]=" +
+				$("mat-mini-container").style.backgroundImage);
+	}
 		
 	// Create a pseudo-list of all tiles in the format
 	// mat[tile][row][column] = "visible;/path/to/tile/source".
+	var tiles = "";
+	for(var i = 0; i != mat.rows.length; ++i) {
+		for(var j = 0; j != mat.rows[i].cells.length; ++j) {
+			// Get visibility. Default is "visible"
+			var visibility = "visible";
+			if(mat.rows[i].cells[j].style.visibility) {
+				visibility = mat.rows[i].cells[j].style.visibility;
+			}
+			
+			// Try to get tile image and its src attribute, but keep a default
+			var uri = "/images/textures/tiles/Spacer.png";
+			var tile = $("mini-mat-tile-" + (i+1) + "-" + (j+1));
+			if(tile) {
+				uri = tile.src;
+			}
+			
+			// Add the & for HTTP POST, but only if neccessary.
+			if((i == 0 && j > 0) || i > 0) {
+				tiles += "&";
+			}
+			tiles += encodeURI("mat[tiles][" + i + "][" + j + "]=" + 
+					visibility + ";" + uri);
+		}
+	}
 	
 	new Ajax.Request(action, {
 		method: method,
-		parameters: Form.serialize(form) + "&" + encodeURI(dimensions),
+		parameters: Form.serialize(form) + "&" + dimensions + "&" + background +
+			"&" + tiles,
 		onFailure: function(transport) {
 			show_ajax_failure_window(transport);
 			document.location.reload();
