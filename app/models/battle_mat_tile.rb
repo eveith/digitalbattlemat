305a6a0cd8d64ee2fb96a0b18d942d33071f80f7
Eric MSP Veith <eveith@wwweb-library.net>
2010-07-23 21:26:21 +0200

    Added constraints to BattleMatTiles model

--- app/models/battle_mat_tile.rb
+++ app/models/battle_mat_tile.rb
@@ -1,3 +1,7 @@
 class BattleMatTile < ActiveRecord::Base
   belongs_to :battle_mat
+
+  validates_numericality_of :x_position, :greater_than_or_equal_to => 0
+  validates_numericality_of :y_position, :greater_than_or_equal_to => 0
+  validates_inclusion_of :visibility, :in => %w(visible hidden)
 end
