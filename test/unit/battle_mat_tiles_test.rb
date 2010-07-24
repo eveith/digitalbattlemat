22624899b353fac4f80a1d40f607e3eb618b314a
Eric MSP Veith <eveith@wwweb-library.net>
2010-07-24 19:51:14 +0200

    Added some model unit tests

--- test/unit/battle_mat_tiles_test.rb
+++ test/unit/battle_mat_tiles_test.rb
@@ -1,8 +1,30 @@
 require 'test_helper'
 
 class BattleMatTilesTest < ActiveSupport::TestCase
-  # Replace this with your real tests.
-  def test_truth
-    assert true
+
+  def test_must_enforce_valid_xy_position
+    t = BattleMatTile.new :x_position => -1, :y_position => -1
+    t.save
+
+    assert t.errors.on(:x_position), 'Must enforce valid X position'
+    assert t.errors.on(:y_position), 'Must enforce valid Y position'
+  end
+
+
+  def test_must_have_a_valid_value_for_visibility
+    t = BattleMatTile.new :visibility => 'visible'
+    t.save
+    assert !t.errors.on(:visibility),
+      'Must allow "visibile" as valid keyword for "visibility"'
+
+    t = BattleMatTile.new :visibility => 'hidden'
+    t.save
+    assert !t.errors.on(:visibility),
+      'Must allow "hidden" as valid keyword for "visibility"'
+
+    t = BattleMatTile.new :visibility => 'Fubar'
+    t.save
+    assert t.errors.on(:visibility),
+      'Must check for a valid value for "visiblity" attribute'
   end
 end
--- test/unit/battle_mats_test.rb
+++ test/unit/battle_mats_test.rb
@@ -1,8 +1,39 @@
 require 'test_helper'
 
 class BattleMatsTest < ActiveSupport::TestCase
-  # Replace this with your real tests.
-  def test_truth
-    assert true
+  def test_must_provide_default_width_and_height
+    b = BattleMat.new
+    b.save
+
+    assert_not_nil b.width, 'Mat must have a default width'
+    assert_not_nil b.height, 'Mat must have a default height'
+  end
+
+
+  def test_must_enforce_valid_width_and_height
+    b = BattleMat.new
+    b.width = -1
+    b.height = -1
+    b.save
+
+    assert b.errors.on(:width), 'Must enforce valid width'
+    assert b.errors.on(:height), 'Must enforce valid height'
+  end
+
+
+  def test_must_delete_all_tiles_on_deletion
+    b = BattleMat.new
+    b.save
+    b.battle_mat_tiles.create :x_position => 0, :y_position => 0
+
+    assert !b.battle_mat_tiles.empty?, 'Must have one tile associated with the mat'
+
+    mat_id = b.id
+
+    b.destroy
+    
+    assert BattleMatTile.find(:all,
+        :conditions => { :battle_mat_id => mat_id }).empty?,
+      'There must be no orphaned tiles after mat deletion'
   end
 end
